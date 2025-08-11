31df9d4739beacbb9cf368fc0e0ee30e196c6e9f
ruvigi <ruvigi@undateable.net>
2025-08-11 03:31:54 +0200

    Version bump

--- root.js
+++ root.js
@@ -2,7 +2,7 @@
 // @name        EmeraldCheat
 // @namespace   http://tampermonkey.net/
 // @match       https://emeraldchat.com/*
-// @version     3.1.4.[filmer timestamp]
+// @version     3.1.5.[filmer timestamp]
 // @author      -
 // @run-at      document-end
 // @description Full UI replacement for EmeraldChat. Open: https://emeraldchat.com/cheat
